f341932a8d5b1e275508c9a768d1b9937b2325ff
Evan Parke <eparke@DESKTOP-IF9RP0H.localdomain>
2022-11-25 01:19:11 -0500

    Got node to run but it says the command roll-dice doesn't exist

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
-import {roll} from "~/workspace/a03-eparkee/lib/roll.js";
+//importing in the libraries and functions
+import roll from "../lib/roll.js";
 import minimist from "minimist";
 
+//creating the string parsing thing
 const args = minimist(process.argv.slice(2));
+
+let sides = 6
+let dice = 2 
+let rolls = 1
 
 //bring in values and set defaults if outside of bounds
 if(args.sides > 0){
@@ -11,6 +17,7 @@ if(args.sides > 0){
 }
 else {
 	sides = 6
+}
 if(args.dice > 0){
 	dice = args.dice
 }
@@ -25,7 +32,6 @@ else{
 }
 
 //run the program from the command line
-console.log(JSON.stringify(roll-dice(sides,dice,rolls)))
+console.log(JSON.stringify(roll(sides,dice,rolls)))
 //exit the program with no status message
-process.exit(0)
-
+process.exit(0);
